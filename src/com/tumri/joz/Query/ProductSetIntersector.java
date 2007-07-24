e452061a4a7a4f78412b1de886d18e83c657b339
snawathe <a@b>
2007-07-25 00:29:43 +0530

    Integrating the changes in.
    [git-p4: depot-paths = "//depot/Tumri/tas/joz/": change = 6031]

---
+++ src/com/tumri/joz/Query/ProductSetIntersector.java
@@ -0,0 +1,27 @@
+package com.tumri.joz.Query;
+
+import com.tumri.joz.products.Handle;
+import com.tumri.joz.utils.Pair;
+import com.tumri.joz.utils.Result;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: snawathe
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProductSetIntersector extends SetIntersector<Handle> {
+  /**
+   * Given a Value v and a score build the Pair object of PID,SCORE
+   * This method should be overridden if default Pair<V,Double> is not acceptable
+   * @param h
+   * @param score
+   * @return a Pair<Pid,Double>
+   */
+  public Result getResult(Handle h, Double score) {
+    return new Result(h.getOid(),score);
+  }
+
+  public ProductSetIntersector() {
+    super();
+  }
+}
