44f6c4ca72adb873108dcc20459e2cdf130cb9c3
ramki <a@b>
2007-10-11 15:50:11 +0530

    refactored with atomic variables.
    [git-p4: depot-paths = "//depot/Tumri/tas/joz/": change = 7615]

--- src/com/tumri/joz/monitor/PerformanceMonitor.java
+++ src/com/tumri/joz/monitor/PerformanceMonitor.java
@@ -5,31 +5,28 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.Iterator;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentHashMap;
 import org.apache.log4j.Logger;
 
 /**
- * Monitor for JoZ Performance statistics by monitoring product requests.
- *
- * @author Ramki
- */
-public class PerformanceMonitor extends ComponentMonitor implements Runnable
+  * JoZ Performance statistics monitor.
+  *
+  * @author Ramki
+  */
+public class PerformanceMonitor extends ComponentMonitor
 {
 	private static PerformanceMonitor instance=null;
-	private long totalRequests=0;
-	private long failedRequests=0;
-	private long minRequestTime=0;
-	private long maxRequestTime=0;
-	private long totalTime=0;
+	private AtomicLong totalRequests=new AtomicLong();
+	private AtomicLong failedRequests=new AtomicLong();
+	private AtomicLong minRequestTime=new AtomicLong();
+	private AtomicLong maxRequestTime=new AtomicLong();
+	private AtomicLong totalTime=new AtomicLong();
 	private AtomicReference minTimedTspecName=new AtomicReference();
 	private AtomicReference maxTimedTspecName=new AtomicReference();
-	private Map<String,Long> failedTspecsMap=new HashMap<String,Long>();
+	private ConcurrentHashMap<String,Long> failedTspecsMap=new ConcurrentHashMap<String,Long>();
 	private Date startDate=new Date();
-	private ConcurrentLinkedQueue<HashMap<String,Long>> reqQueue=new ConcurrentLinkedQueue<HashMap<String,Long>>();
-	private boolean isRunning=false;
-	private boolean isStop=false;
-	private static Thread bgThread=null;
 
     private static Logger log = Logger.getLogger(PerformanceMonitor.class);
 
@@ -43,8 +40,6 @@ public class PerformanceMonitor extends ComponentMonitor implements Runnable
 			synchronized(PerformanceMonitor.class) {
 				if (null == instance) {
 					instance=new PerformanceMonitor();
-					bgThread=new Thread(instance);
-					bgThread.start();
 				}
 			}
 		}
@@ -52,89 +47,48 @@ public class PerformanceMonitor extends ComponentMonitor implements Runnable
 	}
 
 	public void registerSuccess(String tspecName,long elapsedTime) {
-		HashMap<String,Long> req=new HashMap<String,Long>();
-		req.put(tspecName,new Long(elapsedTime));
-		reqQueue.add(req);
+		if (totalRequests.get() == failedRequests.get()) {
+			maxRequestTime.set(elapsedTime);
+			maxTimedTspecName.set((String)tspecName);
+			minRequestTime.set(elapsedTime);
+			minTimedTspecName.set((String)tspecName);
+		}
+		else {
+			if (elapsedTime < minRequestTime.get()) {
+				minRequestTime.set(elapsedTime);
+				minTimedTspecName.set((String)tspecName);
+			}
+			else if ( elapsedTime > maxRequestTime.get()) {
+				maxRequestTime.set(elapsedTime);
+				maxTimedTspecName.set((String)tspecName);
+			}
+		}
+		totalTime.addAndGet(elapsedTime);
+		totalRequests.getAndIncrement();
 	}
 
 	public void registerFailure(String tspecName) {
-		HashMap<String,Long> req=new HashMap<String,Long>();
-		req.put(tspecName,new Long(-1));
-		reqQueue.add(req);
+		failedRequests.getAndIncrement();
+		Long failedCount=(Long)failedTspecsMap.get(tspecName);
+		if (null == failedCount) {
+			failedTspecsMap.put(tspecName,new Long(1));
+		}
+		else {
+			failedTspecsMap.put(tspecName,new Long(failedCount.longValue()+1));
+		}
 	}
 
     public MonitorStatus getStatus(String arg)
     {
-		((PerformanceMonitorStatus)status).setMaxTime(maxRequestTime);
+		((PerformanceMonitorStatus)status).setMaxTime(maxRequestTime.get());
 		((PerformanceMonitorStatus)status).setMaxTspec((String)maxTimedTspecName.get());
-		((PerformanceMonitorStatus)status).setMinTime(minRequestTime);
+		((PerformanceMonitorStatus)status).setMinTime(minRequestTime.get());
 		((PerformanceMonitorStatus)status).setMinTspec((String)minTimedTspecName.get());
-		((PerformanceMonitorStatus)status).setTotalRequestCount(totalRequests);
-		((PerformanceMonitorStatus)status).setFailedRequestCount(failedRequests);
+		((PerformanceMonitorStatus)status).setTotalRequestCount(totalRequests.get());
+		((PerformanceMonitorStatus)status).setFailedRequestCount(failedRequests.get());
 		((PerformanceMonitorStatus)status).setFailedTspecs(failedTspecsMap);
-		((PerformanceMonitorStatus)status).setTotalTime(totalTime);
+		((PerformanceMonitorStatus)status).setTotalTime(totalTime.get());
 		((PerformanceMonitorStatus)status).setStartDate(startDate);
 		return status;
     }
-
-    public void stop() {
-		this.isStop=true;
-		bgThread.interrupt();
-	}
-
-    public void run() {
-		try {
-			log.error("Joz Monitor : Performance Monitor Thread is getting started...");
-			this.isRunning=true;
-			while(this.isStop==false) {
-				HashMap<String,Long> reqMap=(HashMap<String,Long>)reqQueue.poll();
-				if (null == reqMap) {
-					Thread.sleep(1000*5);//Wait for 5 seconds.
-				}
-				else {
-					Set keys=reqMap.keySet();
-					Iterator it=keys.iterator();
-					String tspecName=(String)it.next();
-					Long elapsedTime=reqMap.get(tspecName);
-					log.error("Joz Performance Monitor : Processing t-spec : "+tspecName);
-					if (-1L == elapsedTime) {
-						failedRequests++;
-						totalRequests++;
-						Long failedCount=failedTspecsMap.get(tspecName);
-						if (null == failedCount) {
-							failedTspecsMap.put(tspecName,new Long(1));
-						}
-						else {
-							failedTspecsMap.put(tspecName,new Long(failedCount.longValue()+1));
-						}
-					}
-					else {
-						if (failedRequests == totalRequests) {
-							minRequestTime=elapsedTime;
-							minTimedTspecName.set((String)tspecName);
-							maxRequestTime=elapsedTime;
-							maxTimedTspecName.set((String)tspecName);
-						}
-						else {
-							if (elapsedTime < minRequestTime) {
-								minRequestTime=elapsedTime;
-								minTimedTspecName.set((String)tspecName);
-							}
-							else if (elapsedTime > maxRequestTime) {
-								maxRequestTime=elapsedTime;
-								maxTimedTspecName.set((String)tspecName);
-							}
-						}
-						totalTime=totalTime+elapsedTime;
-						totalRequests++;
-					}
-				}
-			}
-			this.isRunning=false;
-			log.error("Joz Monitor : Performance Monitor Thread stopped. : ");
-		}catch(Exception e) {
-			this.isRunning=false;
-			log.error("Joz Monitor : Performance Monitor Thread terminated. : "+e.getMessage());
-		}
-	}
 }
