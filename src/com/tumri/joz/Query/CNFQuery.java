2916ee377b27a837f13673271e029292a1d67035
nipun <a@b>
2007-11-27 22:24:32 +0530

    Fix for the eliminating additional instances of lack of randomness. Need to also see if there are situations where some products are not seen (such as the first page) since now the handle is always a valid reference
    [git-p4: depot-paths = "//depot/Tumri/tas/joz/": change = 8291]

--- src/com/tumri/joz/Query/CNFQuery.java
+++ src/com/tumri/joz/Query/CNFQuery.java
@@ -45,7 +45,7 @@ public class CNFQuery implements Query, Cloneable {
   public void setReference(Handle aReference) {
     if (aReference!= null && m_cache_reference != null) {
         //Generate a new reference point
-        IProduct iProdHandle = ProductDB.getInstance().get(m_cache_reference.getOid()+1);
+        IProduct iProdHandle = ProductDB.getInstance().get(m_cache_reference.getOid());
         if (iProdHandle!=null) {
             m_reference = iProdHandle.getHandle();
         } else {
