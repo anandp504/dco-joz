86c098bdaa5d76e27d7e45bac24ce508fb39cfda
Scott Braun <scbraun@tumri.com>
2008-10-23 05:11:28 +0530

    JozIndexing Unit Tests updated, for now need to copy files from test/data/testJozIndex/* to /tmp/testJozIndex/* manually...ant script will follow to copy them automatically.
    [git-p4: depot-paths = "//depot/Tumri/tas/joz/": change = 13423]

--- test/com/tumri/joz/index/TestJozIndexCreation.java
+++ test/com/tumri/joz/index/TestJozIndexCreation.java
@@ -1,16 +1,11 @@
 package com.tumri.joz.index;
 
-import com.tumri.joz.utils.FSUtils;
-import com.tumri.joz.products.JozIndexHelper;
-import com.tumri.joz.keywordServer.ProductIndex;
-import com.tumri.joz.index.creator.JozIndexUpdater;
 import com.tumri.joz.index.creator.JozIndexCreator;
-import org.junit.BeforeClass;
+import com.tumri.joz.utils.IndexDebugUtils;
 import org.junit.Test;
-import static org.junit.Assert.assertNotNull;
+import junit.framework.TestCase;
 
 import java.io.File;
-import java.io.IOException;
 
 /**
  * Class to test creation on index from a given MUP
@@ -19,12 +14,17 @@ import java.io.IOException;
  * 3. Do in the indexing.
  * 4. Read the index and make sure that we have the ADD, DELETE and NO CHANGE information matching.
  * 5. Load the index into Joz, and perform the TSpec queries and ensure that the indexes are working.
+ *
+ * contents must be moved from test/data/testJozIndex/* to /tmp/testJozIndex/*
  * @author: nipun
  * Date: Feb 28, 2008
  * Time: 2:37:03 PM
  */
-public class TestJozIndexCreation {
-
+public class TestJozIndexCreation extends TestCase{
+//	private static final String oldDir = "/tmp/testJozIndex/old";
+//	private static final String newDir = "/tmp/testJozIndex/new";
+//	private static final String binDir = "/tmp/testJozIndex/jozIndex";
+	
     private static final String baseDir = "/tmp/joztest";
     private static final String taxonomyDir = "/tmp/taxonomy";
     private static File _baseDir = null;
@@ -48,12 +48,172 @@ public class TestJozIndexCreation {
 //    }
 
     @Test
-    public void testIndexing() {
+    public void testIndexingSameContent() {
         //Do the indexing
-        JozIndexCreator ic = new JozIndexCreator("/tmp/new", "/tmp/old", "/tmp/jozindex", 10000);
+	    String oldDir = "/tmp/testJozIndex/old/old1";
+		String newDir = "/tmp/testJozIndex/new/new1";
+		String binDir = "/tmp/testJozIndex/jozIndex";
+        JozIndexCreator ic = new JozIndexCreator(newDir, oldDir, binDir, 10000);
         ic.createJozIndexes();
-        
-        //TODO: Validate the file.
+	    StringBuffer debugBuff;
+	    IndexDebugUtils debugUtil = new IndexDebugUtils();
+	    String[] args = new String[2];
+	    args[0] = "-binLoc";
+	    args[1] = binDir;
+	    debugBuff = debugUtil.returnBuffer(args);
+
+	    ic = new JozIndexCreator(oldDir, newDir, binDir, 10000);
+	    ic.createJozIndexes();
+	    StringBuffer debugBuff2;
+	    debugBuff2 = debugUtil.returnBuffer(args);
+	    if(!debugBuff.toString().equals(debugBuff2.toString())){
+		    fail("outputs not equal");
+	    }
+    }
+
+	@Test
+    public void testIndexingSameFile() {
+        //Do the indexing
+	    String oldDir = "/tmp/testJozIndex/old/old1";
+		String binDir = "/tmp/testJozIndex/jozIndex";
+        JozIndexCreator ic = new JozIndexCreator(oldDir, oldDir, binDir, 10000);
+        ic.createJozIndexes();
+	    StringBuffer debugBuff;
+	    IndexDebugUtils debugUtil = new IndexDebugUtils();
+	    String[] args = new String[2];
+	    args[0] = "-binLoc";
+	    args[1] = binDir;
+	    debugBuff = debugUtil.returnBuffer(args);
+
+	    ic = new JozIndexCreator(oldDir, oldDir, binDir, 10000);
+	    ic.createJozIndexes();
+	    StringBuffer debugBuff2;
+	    debugBuff2 = debugUtil.returnBuffer(args);
+	    if(!debugBuff.toString().equals(debugBuff2.toString())){
+		    fail("outputs not equal");
+	    }
+    }
+
+	@Test
+    public void testIndexingOneDifference() {
+		String oldDir = "/tmp/testJozIndex/old/old2";
+		String newDir = "/tmp/testJozIndex/new/new2";
+		String binDir = "/tmp/testJozIndex/jozIndex";
+
+        JozIndexCreator ic = new JozIndexCreator(newDir, oldDir, binDir, 10000);
+        ic.createJozIndexes();
+	    StringBuffer debugBuff;
+	    IndexDebugUtils debugUtil = new IndexDebugUtils();
+	    String[] args = new String[2];
+	    args[0] = "-binLoc";
+	    args[1] = binDir;
+	    debugBuff = debugUtil.returnBuffer(args);
+
+	    ic = new JozIndexCreator(oldDir, newDir, binDir, 10000);
+	    ic.createJozIndexes();
+	    StringBuffer debugBuff2;
+	    debugBuff2 = debugUtil.returnBuffer(args);
+	    if(debugBuff.toString().equals(debugBuff2.toString())){
+		    fail("outputs are equal");
+	    }
+		int numAdds1 = countWord(debugBuff.toString(), "ADD");
+		int numDeletes1 = countWord(debugBuff.toString(), "DELETE");
+		int numNoChanges1 = countWord(debugBuff.toString(), "NO-CHANGE");
+
+		int numAdds2 = countWord(debugBuff2.toString(), "ADD");
+		int numDeletes2 = countWord(debugBuff2.toString(), "DELETE");
+		int numNoChanges2 = countWord(debugBuff2.toString(), "NO-CHANGE");
+
+		if(numDeletes1 != 0){
+			fail("num Deletes should be zero for (old, new)");
+		}                                                     
+
+		if(numAdds2 != 0){
+			fail("num Adds should be zero for (new, old)");
+		}
+
+		if(numAdds1 != numDeletes2){
+			fail("num adds for (old, new) should equal num deletes (new, old)");
+		}
+		if(numNoChanges1 != numNoChanges2){
+			fail("num NO-CHANGES are not equal");
+		}
+    }
+
+	@Test
+    public void testIndexingOneReArranged() {
+		String oldDir = "/tmp/testJozIndex/old/old3";
+		String newDir = "/tmp/testJozIndex/new/new3";
+		String binDir = "/tmp/testJozIndex/jozIndex";
+
+        JozIndexCreator ic = new JozIndexCreator(newDir, oldDir, binDir, 10000);
+        ic.createJozIndexes();
+	    StringBuffer debugBuff;
+	    IndexDebugUtils debugUtil = new IndexDebugUtils();
+	    String[] args = new String[2];
+	    args[0] = "-binLoc";
+	    args[1] = binDir;
+	    debugBuff = debugUtil.returnBuffer(args);
+
+	    ic = new JozIndexCreator(oldDir, newDir, binDir, 10000);
+	    ic.createJozIndexes();
+	    StringBuffer debugBuff2;
+	    debugBuff2 = debugUtil.returnBuffer(args);
+	    if(!debugBuff.toString().equals(debugBuff2.toString())){
+		    fail("outputs not equal");
+	    }
+	}
+
+	@Test
+    public void testIndexingAlteredInfo() {
+		String oldDir = "/tmp/testJozIndex/old/old4";
+		String newDir = "/tmp/testJozIndex/new/new4";
+		String binDir = "/tmp/testJozIndex/jozIndex";
+
+        JozIndexCreator ic = new JozIndexCreator(newDir, oldDir, binDir, 10000);
+        ic.createJozIndexes();
+	    StringBuffer debugBuff;
+	    IndexDebugUtils debugUtil = new IndexDebugUtils();
+	    String[] args = new String[2];
+	    args[0] = "-binLoc";
+	    args[1] = binDir;
+	    debugBuff = debugUtil.returnBuffer(args);
+
+	    ic = new JozIndexCreator(oldDir, newDir, binDir, 10000);
+	    ic.createJozIndexes();
+	    StringBuffer debugBuff2;
+	    debugBuff2 = debugUtil.returnBuffer(args);
+	    if(debugBuff.toString().equals(debugBuff2.toString())){
+		    fail("outputs are equal");
+	    }
+		int numAdds1 = countWord(debugBuff.toString(), "ADD");
+		int numDeletes1 = countWord(debugBuff.toString(), "DELETE");
+		int numNoChanges1 = countWord(debugBuff.toString(), "NO-CHANGE");
+		int numAddMods = countWord(debugBuff.toString(), "ADD-MOD");
+		int numDeleteMods = countWord(debugBuff.toString(), "DELETE-MOD");
+
+		int numAdds2 = countWord(debugBuff2.toString(), "ADD");
+		int numDeletes2 = countWord(debugBuff2.toString(), "DELETE");
+		int numNoChanges2 = countWord(debugBuff2.toString(), "NO-CHANGE");
+		int numAddMods2 = countWord(debugBuff.toString(), "ADD-MOD");
+		int numDeleteMods2 = countWord(debugBuff.toString(), "DELETE-MOD");
+
+		if(numDeletes1 != 1){
+			fail("num Deletes should be zero for (old, new)");
+		}
+		if(numDeleteMods != 1 || numDeleteMods != numDeleteMods2){
+			fail("number of delete-mods incorrect");	
+		}
+		if(numAddMods != 1 || numAddMods != numAddMods2){
+			fail("number of add-mods incorrect");
+		}
+
+		if(numAdds2 != 1){
+			fail("num Adds should be zero for (new, old)");
+		}
+		if(numNoChanges1 != numNoChanges2){
+			fail("num NO-CHANGES are not equal");
+		}
     }
 
     @Test
@@ -80,5 +240,15 @@ public class TestJozIndexCreation {
     public void testIncrementalIndexNoCommon() {
 
     }
+
+	private int countWord(String s, String w){
+		int count = 0;
+		int index = 0;
+		while((index = s.indexOf(w, index)) != -1){
+			count++;
+			index++;
+		}
+		return count;
+	}
 
 }
