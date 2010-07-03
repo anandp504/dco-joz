d3b5fee09b3c2360e3b5937eb2adf8b3d6371845
nipun <a@b>
2010-07-03 06:23:39 +0530

    Check for empty listing clause before adding to db.
    [git-p4: depot-paths = "//depot/Tumri/tas/joz/": change = 24606]

--- src/com/tumri/joz/campaign/wm/loader/WMDBLoader.java
+++ src/com/tumri/joz/campaign/wm/loader/WMDBLoader.java
@@ -44,99 +44,103 @@ import org.apache.log4j.Logger;
  */
 public class WMDBLoader {
 
-	private static final Logger log = Logger.getLogger(WMDBLoader.class);
+    private static final Logger log = Logger.getLogger(WMDBLoader.class);
 
-	/**
-	 * Public method to find the wm.xml file and load into the db.
-	 *
-	 * @throws WMLoaderException
-	 */
-	public static List<String> loadData() throws WMLoaderException {
-		String dir = AppProperties.getInstance().getProperty("com.tumri.campaign.wm.xmlFileDir");
-		String pttern = AppProperties.getInstance().getProperty("com.tumri.campaign.wm.xmlFileNamePattern");
+    /**
+     * Public method to find the wm.xml file and load into the db.
+     *
+     * @throws WMLoaderException
+     */
+    public static List<String> loadData() throws WMLoaderException {
+        String dir = AppProperties.getInstance().getProperty("com.tumri.campaign.wm.xmlFileDir");
+        String pttern = AppProperties.getInstance().getProperty("com.tumri.campaign.wm.xmlFileNamePattern");
 
-		File srcDir = new File(dir);
-		ArrayList<File> wmFiles = new ArrayList<File>();
-		ArrayList<String> loadedFiles = new ArrayList<String>();
-		//Check for campaign files
-		if (srcDir.exists() && srcDir.isDirectory()) {
-			findFiles(wmFiles, pttern, srcDir);
-		} else {
-			throw new WMLoaderException("weights matrix source directory is invalid : " + dir);
-		}
+        File srcDir = new File(dir);
+        ArrayList<File> wmFiles = new ArrayList<File>();
+        ArrayList<String> loadedFiles = new ArrayList<String>();
+        //Check for campaign files
+        if (srcDir.exists() && srcDir.isDirectory()) {
+            findFiles(wmFiles, pttern, srcDir);
+        } else {
+            throw new WMLoaderException("weights matrix source directory is invalid : " + dir);
+        }
 
-		if (!wmFiles.isEmpty()) {
-			WMXMLParser parserImpl = getParserImpl();
-			List<String> failedFilesErrors = new ArrayList<String>();
-			boolean error = false;
-			for (File xmlFile : wmFiles) {
-				log.info("Now loading  :" + xmlFile.getAbsolutePath());
-				try {
-					parserImpl.process(xmlFile.getAbsolutePath());
-					loadedFiles.add(xmlFile.getAbsolutePath());
-				} catch (WMLoaderException e) {
+        if (!wmFiles.isEmpty()) {
+            WMXMLParser parserImpl = getParserImpl();
+            List<String> failedFilesErrors = new ArrayList<String>();
+            boolean error = false;
+            for (File xmlFile : wmFiles) {
+                log.info("Now loading  :" + xmlFile.getAbsolutePath());
+                try {
+                    parserImpl.process(xmlFile.getAbsolutePath());
+                    loadedFiles.add(xmlFile.getAbsolutePath());
+                } catch (WMLoaderException e) {
                     log.error("Exception on loading file", e);
-					failedFilesErrors.add("Failed to load : " + xmlFile.getAbsolutePath() + ". Reason : " + e.getMessage());
-					error = true;
-				}
-			}
-			parserImpl.finalize();
-			if (error) {
-				StringBuilder sb = new StringBuilder();
-				sb.append("weights matrix loading encountered the following errors: ");
-				for (String fileMessage : failedFilesErrors) {
-					sb.append(fileMessage);
-					sb.append('\n');
-				}
-				throw new WMLoaderException(sb.toString());
-			}
-		} else {
-			throw new WMLoaderException("No WM files found to load");
-		}
-		return loadedFiles;
-	}
+                    failedFilesErrors.add("Failed to load : " + xmlFile.getAbsolutePath() + ". Reason : " + e.getMessage());
+                    error = true;
+                }
+            }
+            parserImpl.finalize();
+            if (error) {
+                StringBuilder sb = new StringBuilder();
+                sb.append("weights matrix loading encountered the following errors: ");
+                for (String fileMessage : failedFilesErrors) {
+                    sb.append(fileMessage);
+                    sb.append('\n');
+                }
+                throw new WMLoaderException(sb.toString());
+            }
+        } else {
+            throw new WMLoaderException("No WM files found to load");
+        }
+        return loadedFiles;
+    }
 
-	public static void forceLoadData() {
-		try {
-			log.info("Going to force refresh wm data.");
-			long startTime = System.currentTimeMillis();
-			List<String> loadedFiles = loadData();
-			StringBuffer sb = new StringBuffer();
-			if (loadedFiles.size() > 0) {
-				sb.append("Files loaded : ");
-				for (String f : loadedFiles) {
-					sb.append(f);
-					sb.append(",");
-				}
-			}
-			WMContentProviderStatus.getInstance().lastSuccessfulRefreshTime = startTime;
-			WMContentProviderStatus.getInstance().lastRunStatus = true;
-			WMContentProviderStatus.getInstance().addRunHistory(startTime, true, "Force Refresh successful." + sb.toString() +
-					" Time Taken = " + (System.currentTimeMillis() - startTime) + " millis.");
-			WMContentProviderStatus.getInstance().lastRefreshTime = startTime;
-			log.info("WM data force refreshed successfully. Time Taken = " + (System.currentTimeMillis() - startTime) + " millis.");
-		} catch (WMLoaderException e) {
-			log.info("WM data force refresh failed", e);
-			long errTime = System.currentTimeMillis();
-			Writer errorDetails = new StringWriter();
-			PrintWriter pw = new PrintWriter(errorDetails);
-			e.printStackTrace(pw);
-			WMContentProviderStatus.getInstance().addRunHistory(errTime, false, "Refresh Failed. " +
-					" Details : " + errorDetails.toString());
-			WMContentProviderStatus.getInstance().lastError = e;
-			WMContentProviderStatus.getInstance().lastErrorRunTime = errTime;
-			WMContentProviderStatus.getInstance().lastRunStatus = false;
-			WMContentProviderStatus.getInstance().lastRefreshTime = errTime;
-			LogUtils.getFatalLog().fatal("WM data force refresh failed", e);
-		}
+    public static void forceLoadData() {
+        try {
+            log.info("Going to force refresh wm data.");
+            long startTime = System.currentTimeMillis();
+            List<String> loadedFiles = loadData();
+            StringBuffer sb = new StringBuffer();
+            if (loadedFiles.size() > 0) {
+                sb.append("Files loaded : ");
+                for (String f : loadedFiles) {
+                    sb.append(f);
+                    sb.append(",");
+                }
+            }
+            WMContentProviderStatus.getInstance().lastSuccessfulRefreshTime = startTime;
+            WMContentProviderStatus.getInstance().lastRunStatus = true;
+            WMContentProviderStatus.getInstance().addRunHistory(startTime, true, "Force Refresh successful." + sb.toString() +
+                    " Time Taken = " + (System.currentTimeMillis() - startTime) + " millis.");
+            WMContentProviderStatus.getInstance().lastRefreshTime = startTime;
+            log.info("WM data force refreshed successfully. Time Taken = " + (System.currentTimeMillis() - startTime) + " millis.");
+        } catch (WMLoaderException e) {
+            log.info("WM data force refresh failed", e);
+            long errTime = System.currentTimeMillis();
+            Writer errorDetails = new StringWriter();
+            PrintWriter pw = new PrintWriter(errorDetails);
+            e.printStackTrace(pw);
+            WMContentProviderStatus.getInstance().addRunHistory(errTime, false, "Refresh Failed. " +
+                    " Details : " + errorDetails.toString());
+            WMContentProviderStatus.getInstance().lastError = e;
+            WMContentProviderStatus.getInstance().lastErrorRunTime = errTime;
+            WMContentProviderStatus.getInstance().lastRunStatus = false;
+            WMContentProviderStatus.getInstance().lastRefreshTime = errTime;
+            LogUtils.getFatalLog().fatal("WM data force refresh failed", e);
+        }
 
-	}
+    }
 
-	public static void updateDb(int expId, SortedBag<Pair<CreativeSet, Double>> optRules,
+    public static void updateDb(int expId, SortedBag<Pair<CreativeSet, Double>> optRules,
                                 SortedBag<Pair<ListingClause, Double>> lcRules,
                                 Map<VectorAttribute,  List<Integer>> requestMap, VectorHandle handle) {
-        VectorDB.getInstance().addClause(handle.getOid(), lcRules);
-        VectorDB.getInstance().addRules(handle.getOid(), optRules);
+        if (lcRules!=null){
+            VectorDB.getInstance().addClause(handle.getOid(), lcRules);
+        }
+        if (optRules!=null){
+            VectorDB.getInstance().addRules(handle.getOid(), optRules);
+        }
 
         TreeMap<Integer, ArrayList<Handle>> idmap = new TreeMap<Integer, ArrayList<Handle>>();
         ArrayList<Handle> idHandles = new ArrayList<Handle>();
@@ -144,9 +148,9 @@ public class WMDBLoader {
         idmap.put(expId, idHandles);
         VectorDB.getInstance().updateIntegerIndex(VectorAttribute.kExpId, idmap);
 
-		if (requestMap != null && !requestMap.isEmpty()) {
-			for (VectorAttribute attr : requestMap.keySet()) {
-				List<Integer> idList = requestMap.get(attr);
+        if (requestMap != null && !requestMap.isEmpty()) {
+            for (VectorAttribute attr : requestMap.keySet()) {
+                List<Integer> idList = requestMap.get(attr);
                 for (Integer id: idList) {
                     if (id != null) {
 
@@ -190,49 +194,49 @@ public class WMDBLoader {
                 map.put(id, handles);
                 VectorDB.getInstance().updateIntegerIndex(nAttr, map);
             }
-            
-		}
-	}
 
-	private static WMXMLParser getParserImpl() {
-		WMXMLParser impl = new WMXMLParserV1();
-		return impl;
-	}
+        }
+    }
 
-	/**
-	 * Searches the list of campaign files to be loaded based on search directory and filename pattern
-	 *
-	 * @param matchedFilesAL list of matched files
-	 * @param filePattern    pattern to be searched
-	 * @param dir            direcory to look for the files specified by the filePattern
-	 */
-	private static void findFiles(ArrayList<File> matchedFilesAL, String filePattern, File dir) {
-		ArrayList<File> directories = new ArrayList<File>();
-		if (dir.isDirectory()) {
-			String[] children = dir.list();
-			Arrays.sort(children);
-			for (String child : children) {
-				File file = new File(dir.getAbsolutePath() + File.separator + child);
-				if (!file.isDirectory()) {
-					if (file.getName().matches(filePattern)) {
-						matchedFilesAL.add(file);
-					}
-				} else {
-					directories.add(file);
-				}
-			}
-		}
-		for (File f : directories) {
-			findFiles(matchedFilesAL, filePattern, f);
-		}
-	}
+    private static WMXMLParser getParserImpl() {
+        WMXMLParser impl = new WMXMLParserV1();
+        return impl;
+    }
 
-	public static void main(String[] args) {
-		try {
-			loadData();
-		} catch (WMLoaderException e) {
-			e.printStackTrace();
-		}
-	}
+    /**
+     * Searches the list of campaign files to be loaded based on search directory and filename pattern
+     *
+     * @param matchedFilesAL list of matched files
+     * @param filePattern    pattern to be searched
+     * @param dir            direcory to look for the files specified by the filePattern
+     */
+    private static void findFiles(ArrayList<File> matchedFilesAL, String filePattern, File dir) {
+        ArrayList<File> directories = new ArrayList<File>();
+        if (dir.isDirectory()) {
+            String[] children = dir.list();
+            Arrays.sort(children);
+            for (String child : children) {
+                File file = new File(dir.getAbsolutePath() + File.separator + child);
+                if (!file.isDirectory()) {
+                    if (file.getName().matches(filePattern)) {
+                        matchedFilesAL.add(file);
+                    }
+                } else {
+                    directories.add(file);
+                }
+            }
+        }
+        for (File f : directories) {
+            findFiles(matchedFilesAL, filePattern, f);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            loadData();
+        } catch (WMLoaderException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
