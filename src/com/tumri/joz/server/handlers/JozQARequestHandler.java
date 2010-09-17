66461a5f75715fdd2151285a08b15b4d8d17da64
Scott Braun <scbraun@tumri.com>
2010-09-18 02:10:31 +0530

    fix for bug 4318: NPE thrown when joz QA report is displayed.
    [git-p4: depot-paths = "//depot/Tumri/tas/joz/": change = 25771]

--- src/com/tumri/joz/server/handlers/JozQARequestHandler.java
+++ src/com/tumri/joz/server/handlers/JozQARequestHandler.java
@@ -32,44 +32,44 @@ import java.util.*;
 public class JozQARequestHandler implements RequestHandler {
 
 
-	private static Logger log = Logger.getLogger (JozQARequestHandler.class);
+	private static Logger log = Logger.getLogger(JozQARequestHandler.class);
 
 	public QueryResponseData query(QueryInputData input) throws InvalidRequestException {
 		JozQAResponseWrapper responseWrapper = new JozQAResponseWrapper();
-		doQuery((JozQARequest)input, responseWrapper);
+		doQuery((JozQARequest) input, responseWrapper);
 		return responseWrapper;
 	}
 
 	public void query(QueryInputData input, QueryResponseData response) throws InvalidRequestException {
 		try {
-			doQuery((JozQARequest)input, (JozQAResponseWrapper)response);
+			doQuery((JozQARequest) input, (JozQAResponseWrapper) response);
 		} catch (ClassCastException ex) {
-			throw new InvalidRequestException("Only type JozQARequest & JozQAResponse supported.",ex);
+			throw new InvalidRequestException("Only type JozQARequest & JozQAResponse supported.", ex);
 		}
 	}
 
 	public QueryId[] getSupportedIds() {
-		return  new QueryId[]{ QueryId.JOZ_QA };
+		return new QueryId[]{QueryId.JOZ_QA};
 	}
 
 	public void doQuery(JozQARequest input, JozQAResponseWrapper responseWrapper) {
 		try {
 			processRequest(input, responseWrapper);
-		} catch(JoZException e) {
-			responseWrapper.addDetails(JozQAResponseWrapper.KEY_ERROR,"Joz exception on processing the request : " + e.getMessage());
-			log.error("Jozexception caught",e);
+		} catch (JoZException e) {
+			responseWrapper.addDetails(JozQAResponseWrapper.KEY_ERROR, "Joz exception on processing the request : " + e.getMessage());
+			log.error("Jozexception caught", e);
 		} catch (Throwable t) {
-			responseWrapper.addDetails(JozQAResponseWrapper.KEY_ERROR,"Unexpected exception on processing the request : " + t.getMessage());
-			log.error("Unexpected Exception caught",t);
+			responseWrapper.addDetails(JozQAResponseWrapper.KEY_ERROR, "Unexpected exception on processing the request : " + t.getMessage());
+			log.error("Unexpected Exception caught", t);
 		}
 	}
 
 	/**
-	 *  For each advertisers, Joz will execute down through the campaign hierarchy (Campaign-->AdPod-->Recipe-->ListingQuery).
-	 *  There are three basic states that a recipe can fall into: successful, failed, warned.
-	 *  The success and failure of a Recipe is dependent upon the success and failure of their respective ListingQueries?.
-	 *  Below is a description about the conditions in which Recipes and ListingQueries? will be failed and/or warned.
-	 *
+	 * For each advertisers, Joz will execute down through the campaign hierarchy (Campaign-->AdPod-->Recipe-->ListingQuery).
+	 * There are three basic states that a recipe can fall into: successful, failed, warned.
+	 * The success and failure of a Recipe is dependent upon the success and failure of their respective ListingQueries?.
+	 * Below is a description about the conditions in which Recipes and ListingQueries? will be failed and/or warned.
+	 * <p/>
 	 * Recipe Failures
 	 * A whole Campaign can be failed, resulting in each of it's Recipes being failed, if it hasn't begun yet or is over (flightEnd and flightStart are used to determine Campaign duration).
 	 * A Recipe is failed if any of its ListingQueries? are failed.
@@ -80,11 +80,12 @@ public class JozQARequestHandler implements RequestHandler {
 	 * ListingQuery Warned:
 	 * If its applyGeoFilter or geoEnabledFlagset is set to true.
 	 * When a Recipe is warned a short description will be included within the corresponding QARecipeResponse detailing the reason for the warning. Warned TSpecs will not result in a Recipe failure and Warned Recipes will not result in the overall failure of an Advertiser.
-	 * @param query JozQARequest
+	 *
+	 * @param query           JozQARequest
 	 * @param responseWrapper JozQAResponseWrapper
 	 * @throws com.tumri.joz.JoZException
 	 */
-	private void processRequest(JozQARequest query, JozQAResponseWrapper responseWrapper) throws JoZException{
+	private void processRequest(JozQARequest query, JozQAResponseWrapper responseWrapper) throws JoZException {
 		ArrayList<String> advertisers = new ArrayList<String>();
 		boolean success = true;
 		boolean zeroInitAdv = false;
@@ -94,7 +95,7 @@ public class JozQARequestHandler implements RequestHandler {
 		JozQAResponse resp = new JozQAResponse();
 
 		//construct List of request Advertisers from comma seperated string of names
-		if(advertiserNamesString != null && !"".equals(advertiserNamesString.trim())){
+		if (advertiserNamesString != null && !"".equals(advertiserNamesString.trim())) {
 			StringTokenizer tokenizer = new StringTokenizer(advertiserNamesString, ',');
 			advertisers = tokenizer.getTokens();
 		} else {
@@ -103,34 +104,39 @@ public class JozQARequestHandler implements RequestHandler {
 
 		//do testing of recipes for request advertisers
 		ArrayList<Campaign> camps = CampaignDB.getInstance().getCampaigns();
-		for(Campaign camp:camps){ //for each campaign with a corrosponding clientName
+		for (Campaign camp : camps) { //for each campaign with a corrosponding clientName
 			String clientName = camp.getClientName();
 
-			if(zeroInitAdv){
-				if(clientName == null){
+			if (zeroInitAdv) {
+				if (clientName == null) {
 					log.error("No Client Name: Campaign Name = " + camp.getName() + "; Id = " + camp.getId());
 					continue;
 				}
-				if(!advertisers.contains(clientName)){
+				if (!advertisers.contains(clientName)) {
 					advertisers.add(clientName);
 				}
 			}
-			if(advertisers.contains(clientName)){
-				if(validAdvertisers == null){
+			if (advertisers.contains(clientName)) {
+				if (validAdvertisers == null) {
 					validAdvertisers = new HashSet<String>();
 				}
 				validAdvertisers.add(clientName);
 				Date today = new Date();
 				boolean failCampaign = false;
-				if(camp.getFlightEnd() == null || camp.getFlightStart() == null|| camp.getFlightStart().after(today)
-						|| camp.getFlightEnd().before(today)){
+				if (camp.getFlightEnd() == null || camp.getFlightStart() == null || camp.getFlightStart().after(today)
+						|| camp.getFlightEnd().before(today)) {
 					failCampaign = true;
 				}
 				List<AdPod> adPods = camp.getAdpods();
-				for(AdPod adPod: adPods){  //for each adPod within each camp
-					List<Recipe> recipes = adPod.getRecipes();
-					for(Recipe recipe : recipes){ //for each recipe
-						success = executeRecipe(success, resp, camp, today, failCampaign, adPod, recipe);
+
+				if (adPods != null) {
+					for (AdPod adPod : adPods) {  //for each adPod within each camp
+						List<Recipe> recipes = adPod.getRecipes();
+						if (recipes != null) {
+							for (Recipe recipe : recipes) { //for each recipe
+								success = executeRecipe(success, resp, camp, today, failCampaign, adPod, recipe);
+							}
+						}
 					}
 				}
 			}
@@ -138,12 +144,12 @@ public class JozQARequestHandler implements RequestHandler {
 
 		//construct List of invalid advertisers from diff between valid advertisers and all advertisers
 		ArrayList<String> invalidAdvertisers = null;
-		for(String adv: advertisers){
-			if(validAdvertisers == null || !validAdvertisers.contains(adv)){
-				if(invalidAdvertisers == null){
+		for (String adv : advertisers) {
+			if (validAdvertisers == null || !validAdvertisers.contains(adv)) {
+				if (invalidAdvertisers == null) {
 					invalidAdvertisers = new ArrayList<String>();
 				}
-				resp.addDetail("Invalid-Advertiser="+adv);
+				resp.addDetail("Invalid-Advertiser=" + adv);
 			}
 		}
 
@@ -157,13 +163,14 @@ public class JozQARequestHandler implements RequestHandler {
 
 	/**
 	 * Used by processRequest to execute the various TSpecs found within a specified Recipe.
-	 * @param success boolean, overall success of all advertisers
-	 * @param resp JozQAResponse, Primary interface class that returns all QA info
-	 * @param camp Campaign, in which recipe is located
-	 * @param today Date
+	 *
+	 * @param success      boolean, overall success of all advertisers
+	 * @param resp         JozQAResponse, Primary interface class that returns all QA info
+	 * @param camp         Campaign, in which recipe is located
+	 * @param today        Date
 	 * @param failCampaign boolean, if the entire Campaign and all sub-recipes should fail
-	 * @param adPod AdPod, in which recipe is located
-	 * @param recipe Recipe, all TSpec specified within <RecipeTSpecInfo> should be tested
+	 * @param adPod        AdPod, in which recipe is located
+	 * @param recipe       Recipe, all TSpec specified within <RecipeTSpecInfo> should be tested
 	 * @return true for success
 	 */
 	private boolean executeRecipe(boolean success, JozQAResponse resp, Campaign camp, Date today, boolean failCampaign, AdPod adPod, Recipe recipe) {
@@ -178,11 +185,11 @@ public class JozQARequestHandler implements RequestHandler {
 		recipeResponse.setRecipeId(recipe.getId());
 		recipeResponse.setRecipeName(recipe.getName());
 		//if campaign is invalid: ie not running
-		if(failCampaign){
-			if(camp.getFlightEnd() == null || camp.getFlightEnd().before(today)){
+		if (failCampaign) {
+			if (camp.getFlightEnd() == null || camp.getFlightEnd().before(today)) {
 				recipeResponse.addDetail("Failed: Campaign Flight is over.");
 			}
-			if(camp.getFlightStart() == null || camp.getFlightStart().after(today)){
+			if (camp.getFlightStart() == null || camp.getFlightStart().after(today)) {
 				recipeResponse.addDetail("Failed: Campaign Flight hasn't begun.");
 			}
 			resp.registerRecipeFailure(clientName, recipeResponse);
@@ -190,15 +197,15 @@ public class JozQARequestHandler implements RequestHandler {
 			return success;
 		}
 
-		if(recipe.getWeight() == 0){
+		if (recipe.getWeight() == 0) {
 			resp.registerRecipeSuccess(clientName);
 			return success;
 		}
-		for(RecipeTSpecInfo tSpecInfo: tSpecInfos){ //for each listing query
+		for (RecipeTSpecInfo tSpecInfo : tSpecInfos) { //for each listing query
 			int numReqProducts = tSpecInfo.getNumProducts();
 			int tSpecId = tSpecInfo.getTspecId();
 			TSpec eTSpec = CampaignDB.getInstance().getTspec(tSpecId);
-			if(eTSpec == null){
+			if (eTSpec == null) {
 				eTSpec = new TSpec();
 				eTSpec.setId(tSpecId);
 				nullTSpec = true;
@@ -207,7 +214,7 @@ public class JozQARequestHandler implements RequestHandler {
 
 			boolean geoEnabledTSpec = false;
 			//Geo-Enabled check
-			if(eTSpec.isGeoEnabledFlag() || eTSpec.isApplyGeoFilter()){
+			if (eTSpec.isGeoEnabledFlag() || eTSpec.isApplyGeoFilter()) {
 				geoEnabledTSpec = true;
 				geoEnabled = true;
 				error.addDetail("TSpec is geo enabled and was not processed.");
@@ -215,12 +222,12 @@ public class JozQARequestHandler implements RequestHandler {
 				recipeResponse.addJozQAWarning(error);
 			}
 			//Check if Recipe requests 0 products for its TSpecs
-			if(!(numReqProducts == 0)){
+			if (!(numReqProducts == 0)) {
 				allSpecs0 = false;
 			}
 			//conditions under which we do not want to execute the TSpec
-			if(geoEnabledTSpec || allSpecs0 || nullTSpec){
-				if(nullTSpec){
+			if (geoEnabledTSpec || allSpecs0 || nullTSpec) {
+				if (nullTSpec) {
 					recipeResponse.addFailedTSpec(eTSpec);
 					recipeResponse.addJozQAError(error);
 					error.addDetail("No TSpec with given Id found in campaignDB.");
@@ -229,32 +236,32 @@ public class JozQARequestHandler implements RequestHandler {
 			}
 			//execute TSpec
 			int numReturnedProds = getNumProducts(tSpecId, numReqProducts);
-			if(numReturnedProds < numReqProducts){
+			if (numReturnedProds < numReqProducts) {
 				error.setNumRecieved(numReturnedProds);
 				recipeResponse.addFailedTSpec(eTSpec);
 				recipeResponse.addJozQAError(error);
 				recipeFail = true;
 			}
 		}//end TSpec for
-		if(allSpecs0){
+		if (allSpecs0) {
 			recipeWarn = true;
 			recipeResponse.addDetail("Warn: Recipe has positive weight; however, all TSpecs request 0 products.");
 		}
-		if(geoEnabled){
+		if (geoEnabled) {
 			recipeWarn = true;
 			recipeResponse.addDetail("Warn: Recipe has TSpecs which are Geo-Enabled and were not processed.");
 		}
-		if(nullTSpec){
+		if (nullTSpec) {
 			recipeFail = true;
 			recipeResponse.addDetail("TSpec Failed: No TSpec with specified Id found.");
 		}
 
 		//add RecipeResponse according to status: failed, warned, successful.
-		if(recipeFail){//set response with collected data for each recipe
+		if (recipeFail) {//set response with collected data for each recipe
 			success = false;
 			resp.registerRecipeFailure(clientName, recipeResponse);
-		} else if(recipeWarn){
-		    resp.registerRecipeWarning(clientName, recipeResponse);
+		} else if (recipeWarn) {
+			resp.registerRecipeWarning(clientName, recipeResponse);
 		} else {
 			resp.registerRecipeSuccess(clientName);
 		}
@@ -263,11 +270,12 @@ public class JozQARequestHandler implements RequestHandler {
 
 	/**
 	 * This method is used to test a particular TSpec Id.
-	 * @param tSpecId TSpec Id that will be executed
+	 *
+	 * @param tSpecId  TSpec Id that will be executed
 	 * @param numProds int, The number of desired products
 	 * @return
 	 */
-	private int getNumProducts(int tSpecId, int numProds){
+	private int getNumProducts(int tSpecId, int numProds) {
 		ProductSelectionRequest pr = new ProductSelectionRequest();
 
 		pr.setPageSize(numProds);
@@ -277,17 +285,17 @@ public class JozQARequestHandler implements RequestHandler {
 		pr.setBRandomize(false);
 		pr.setRequestKeyWords(null);
 		pr.setBMineUrls(false);
-		
+
 		ArrayList<Handle> handles;
 		int numReturnedProds = -1;
 
 		try {
-			handles = doProductSelection(tSpecId, pr, new Features() );
-			if(handles != null){
+			handles = doProductSelection(tSpecId, pr, new Features());
+			if (handles != null) {
 				numReturnedProds = handles.size();
 			}
-		} catch(Exception ex) {
-			log.error("Error getting number of prducts: "+ex.getMessage());
+		} catch (Exception ex) {
+			log.error("Error getting number of prducts: " + ex.getMessage());
 		}
 
 		return numReturnedProds;
@@ -295,9 +303,10 @@ public class JozQARequestHandler implements RequestHandler {
 
 	/**
 	 * returns a List of Handles.  Used by getNumProducts to get products for tspecId
+	 *
 	 * @param tspecId int, Id of TSpec to be executed
-	 * @param pr ProductSelectionRequest
-	 * @param f Features
+	 * @param pr      ProductSelectionRequest
+	 * @param f       Features
 	 * @return an ArrayList of Handle Objects representing products return by executing tspecId
 	 */
 	private ArrayList<Handle> doProductSelection(int tspecId, ProductSelectionRequest pr, Features f) {
