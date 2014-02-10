641cab88cfe3fdf4b758fa4f0ffb44f2f7f23c05
Om Prakash <omprakash@collective.com>
2014-02-10 14:25:45 +0530

    revert the intermediate changes, made for DAS-405. this is required as it can affect the current build of joz and utils.
    [git-p4: depot-paths = "//depot/Tumri/tas/joz/": change = 37900]

--- src/com/tumri/joz/products/Handle.java
+++ src/com/tumri/joz/products/Handle.java
@@ -8,7 +8,7 @@ import java.util.Comparator;
  * User: snawathe
  * To change this template use File | Settings | File Templates.
  */
-public interface Handle extends Comparable, Comparator, Serializable{
+public interface Handle extends Comparable, Comparator{
   /**
    * Returns the underlying object id to this handle
    * @return
